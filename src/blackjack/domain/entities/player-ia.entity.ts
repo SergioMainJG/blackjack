a0716547ae482d59ffc8b727432ae09d02d7c613
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-12-07 22:34:22 -0600

    update: player-ia: PlayerIA: shouldRequestOtherCard()

--- src/blackjack/domain/entities/player-ia.entity.ts
+++ src/blackjack/domain/entities/player-ia.entity.ts
@@ -7,9 +7,9 @@ export class PlayerIA extends PlayerDecorator {
     }
 
     shouldRequestOtherCard(opponentPoints: number): boolean {
-        return (21 - opponentPoints >= 10)
-            ? true
-            : false
-
+        if (opponentPoints < 17 && this.player.getScore() < 17) return true;
+        if ((opponentPoints >= 17 && opponentPoints <= 21) && this.player.getScore() < 21) return true;
+        if (this.player.getScore() < 17) return true;
+        return false;
     }
 }
