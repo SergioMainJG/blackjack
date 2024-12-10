a91747a8312c89b349d4831273a85b1618121922
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-12-09 22:29:47 -0600

    Before: optimize project

--- src/blackjack/presentation/game.ts
+++ src/blackjack/presentation/game.ts
@@ -1,25 +1,115 @@
 import { BasePlayer, PlayerIA, createDeck, getCard, randomizeCards } from "../../blackjack";
 
-function addCard (card: string){
-    return `<img class="deck" src="/imgs/cards/${card}.avif" alt="backside-deck" />`
+function addCard(card: string): HTMLImageElement {
+    const cardImg: HTMLImageElement = document.createElement('img');
+    cardImg.src = `/imgs/cards/${card}.avif`;
+    cardImg.classList.add('deck');
+    cardImg.alt = `card: ${card}`;
+    return cardImg;
+}
+type ResultGame = 'ComputerWin' | 'UserWin' | 'Tie'
+function areWinner(points: number[]): ResultGame {
+    const [userPoints, comPoints] = points;
+    if (userPoints === comPoints) return 'Tie';
+    if (21 >= userPoints && comPoints < userPoints) return 'UserWin';
+    if (21 < comPoints && userPoints <= 21) return 'UserWin';
+    return 'ComputerWin';
 }
 
+
 export const game = () => {
-    const deck = randomizeCards(createDeck());
-    const playerHuman = new BasePlayer();
-    const pcPlayer = new PlayerIA(new BasePlayer());
+    let deck = randomizeCards(createDeck());
+    const user = new BasePlayer();
+    const computer = new PlayerIA(new BasePlayer());
+    let auxScoreCom: [number, number] = [0, 0];
+    const hit: HTMLButtonElement = document.querySelector('#hit')!;
+    const stand: HTMLButtonElement = document.querySelector('#stand')!;
+    const surrender: HTMLButtonElement = document.querySelector('#surrender')!;
+    const newGame: HTMLButtonElement = document.querySelector('#newGame')!;
 
-    let isWinner = false;
-    let isStand = false;
+    function endGame(result: ResultGame, points: number[]) {
+        const [userPoints, comPoints] = points;
+        stand.classList.add('hidden');
+        hit.classList.add('hidden');
+        surrender.classList.add('hidden');
+        if (result === 'ComputerWin') window.alert(`You Lose!😥 You can do it best the next time! 🍀; Computer Score: ${comPoints}; User Score: ${userPoints}`);
+        if (result === 'UserWin') window.alert(`You Win!😎You are the best! 😉;  Computer Score: ${comPoints}; User Score: ${userPoints}`);
+        if (result === 'Tie') window.alert(`Tie!😶 this game was close between you 2 😯;  Computer Score: ${comPoints}; User Score: ${userPoints}`)
+    }
 
-    const { card, isEnd, value } = getCard( deck );
-    
+
     const userCards: HTMLDivElement = document.querySelector('#user-cards')!;
     const userScore: HTMLHeadingElement = document.querySelector('#score')!;
-    const pcCards: HTMLDivElement = document.querySelector('#pc-cards')!;
-    const pcScore: HTMLHeadingElement = document.querySelector('#pc-score')!;
+    const comCards: HTMLDivElement = document.querySelector('#pc-cards')!;
+    const comScore: HTMLHeadingElement = document.querySelector('#pc-score')!;
 
-    playerHuman.setScore( value );
-    userScore.textContent! = playerHuman.getScore().toString();
-    pcScore.textContent! = pcPlayer.getScore().toString();
+    user.setScore(getCard(deck).value);
+    auxScoreCom[0] = getCard(deck).value;
+    let isStandPc = false;
+
+    userScore.innerText = auxScoreCom[0].toString();
+    comScore.innerText = computer.getScore().toString();
+
+    hit.addEventListener('click', () => {
+        const pointsUser = getCard(deck);
+        user.setScore(pointsUser.value);
+        userScore.innerText = user.getScore().toString();
+        userCards.append(addCard(pointsUser.card));
+        if (isStandPc) {
+            endGame(
+                areWinner([user.getScore(), computer.getScore()]),
+                [user.getScore(), computer.getScore()]
+            );
+            return;
+        }
+        const comDecision = computer.shouldRequestOtherCard(user.getScore());
+        if (comDecision) {
+            const pointsPC = getCard(deck);
+            computer.setScore(pointsPC.value);
+            auxScoreCom[1] += pointsPC.value;
+            comScore.innerText = auxScoreCom[1].toString();
+            comCards.append(addCard(pointsPC.card));
+        }
+        else isStandPc = true;
+    });
+
+    stand.addEventListener('click', () => {
+        if (isStandPc) {
+            endGame(
+                areWinner([user.getScore(), computer.getScore()]),
+                [user.getScore(), computer.getScore()]
+            );
+            return;
+        }
+        const comDecision = computer.shouldRequestOtherCard(user.getScore());
+        if (comDecision) {
+            const pointsPC = getCard(deck);
+            computer.setScore(pointsPC.value);
+            auxScoreCom[1] += pointsPC.value;
+            comScore.innerText = auxScoreCom[1].toString();
+            comCards.append(addCard(pointsPC.card));
+        }
+        endGame(
+            areWinner([user.getScore(), computer.getScore()]),
+            [user.getScore(), computer.getScore()]
+        );
+        return;
+    });
+
+    surrender.addEventListener('click', () => {
+        window.alert(`You gave up!😥 You can do it best the next time!🍀;`);
+        stand.classList.add('hidden');
+        hit.classList.add('hidden');
+        surrender.classList.add('hidden');
+    });
+    newGame.addEventListener(('click'), () => {
+        user.reset();
+        computer.reset();
+        comCards.innerHTML = `<img src="/imgs/cards/grey_back.avif" alt="card: card's back" class="deck">`;
+        userCards.innerHTML = `<img src="/imgs/cards/grey_back.avif" alt="card: card's back" class="deck">`;
+        auxScoreCom = [0, 0];
+        userScore.innerText = '0';
+        comScore.innerText = '0';
+        deck = randomizeCards(createDeck());
+    });
 }
